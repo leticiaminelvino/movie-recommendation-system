18f247b5cf7e29e75945ab7896a5372c63e243cc
Leticia_03 <>
2023-01-29 05:38:38 +0000

    Initial commit

---
+++ dicionario.py
@@ -0,0 +1,174 @@
+users = {
+  'Almeida': {
+    'Toy Story': 5.0,
+    'Frozen': 4.0,
+    'Spider-Man: Into the Spider-Verse': 4.0,
+    'The Lion King': 5.0
+  },
+  'Ana': {
+    'Frozen': 5.0,
+    'Up': 5.0,
+    'The Lion King': 4.0,
+    'Coco': 5.0,
+    'The Incredibles': 4.0,
+    'The Incredibles 2': 4.0,
+    'The Little Mermaid': 5.0
+  },
+  'Bianca': {
+    'Inception': 5.0,
+    'The Matrix': 4.0,
+    'The Terminator': 5.0
+  },
+  'Candece': {
+    'Star Wars: A New Hope': 5.0,
+    'Blade Runner': 5.0,
+    'The Empire Strikes Back': 4.0,
+    'Return of the Jedi': 3.0
+  },
+  'Carlos': {
+    'Die Hard': 5.0,
+    'Lethal Weapon': 4.0,
+    'Bad Boys': 3.0,
+    'The Terminator': 5.0,
+    'The Terminator 2': 5.0,
+    'Avengers:Endgame': 5.0,
+    'Fast and Furious': 5.0
+  },
+  'Clara': {
+    'Jurassic Park': 5.0,
+    'The Shawshank Redemption': 5.0,
+    'The Exorcist': 4.0,
+    'Alien': 5.0,
+    'The Fault in Our Stars': 3.0,
+    'Titanic': 5.0
+  },
+  'Daniel': {
+    'Up': 4.0,
+    'The Shawshank Redemption': 5.0,
+    'The Dark Knight': 4.0,
+    'The Lion King': 5.0,
+    'Toy Story': 4.0,
+    'The Incredibles': 5.0,
+    'Inside Out': 5.0,
+    'The Lion King 2': 4.0,
+    'Inception': 5.0,
+    'The Conjuring': 3.0,
+    'Halloween': 4.0,
+    'The Notebook': 4.0
+  },
+  'Duarte': {
+    'The Silence of the Lambs': 4.0,
+    'Jaws': 4.0,
+    'Halloween': 5.0,
+    'The Notebook': 4.0,
+    'The Exorcist': 5.0,
+    'The Texas Chain Saw Massacre': 4.0
+  },
+  'Fabiana': {
+    'Frozen': 5.0,
+    'Taken': 4.0,
+    'Lethal Weapon': 4.0,
+    'The Terminator': 5.0,
+    'John Wick': 5.0,
+    'The Bourne Identity': 4.0,
+    'Mission Impossible': 5.0,
+    'The Lion King': 5.0,
+    'Avengers: Endgame': 4.0,
+    'The Silence of the Lambs': 5.0,
+    "To All the Boys I've Loved Before": 3.0,
+    'The Exorcist': 4.0,
+    'The Shining': 5.0,
+    'The Notebook': 3.0
+  },
+  'Ferb': {
+    'Titanic': 5.0,
+    'The Notebook': 5.0,
+    'A Star is Born': 4.0
+  },
+  'Gabriela': {
+    'Star Wars: A New Hope': 5.0,
+    'Star Wars': 5.0,
+    'Blade Runner': 4.0,
+    'The Matrix': 5.0
+  },
+  'Isabele': {
+    'The Wizard of Oz': 5.0,
+    'The Expendables': 4.0,
+    'Get Out': 4.0,
+    'Twilight': 2.0
+  },
+  'Jessica': {
+    'Alien': 4.0,
+    'Gravity': 5.0,
+    'Interstellar': 5.0,
+    'The Thing': 4.0,
+    'Predator': 3.0
+  },
+  'Maria': {
+    'It': 4.0,
+    'The Conjuring': 5.0,
+    'The Exorcist': 4.0,
+    'Get Out': 5.0,
+    'Halloween': 3.0,
+    'A Quiet Place': 4.0
+  },
+  'Mateus': {
+    'Star Wars: A New Hope': 5.0,
+    '2001: A Space Odyssey': 4.0,
+    'The Martian': 5.0,
+    'Interstellar': 4.0,
+    'Alien': 5.0
+  },
+  'Mike': {
+    'Jurassic Park': 4.0,
+    'The Terminator': 5.0,
+    'Alien': 4.0,
+    'The Thing': 3.0,
+    'Predator': 4.0
+  },
+  'Murilo': {
+    'The Andromeda Strain': 4.0,
+    'Solaris': 3.0,
+    'Star Wars: A New Hope': 5.0,
+    "The Hitchhiker's Guide to the Galaxy": 5.0
+  },
+  'Paula': {
+    'Finding Nemo': 5.0,
+    'The Incredibles': 4.0,
+    'A Quiet Place': 4.0,
+    'The Notebook': 5.0
+  },
+  'Paulinha': {
+    'The Empire Strikes Back': 5.0,
+    'Guardians of the Galaxy': 4.0,
+    'Star Trek: The Motion Picture': 3.0,
+    'Star Wars: A New Hope': 5.0
+  },
+  'Perry': {
+    'The Godfather': 4.0,
+    'The Shawshank Redemption': 5.0,
+    'The Dark Knight': 5.0
+  },
+  'Phineas': {
+    'Halloween': 4.0,
+    'The Exorcist': 5.0,
+    'Get Out':4.5,
+    'The Silence of the Lambs': 5.0
+  },
+  'Renato': {
+    'Titanic': 5.0,
+    'The Notebook': 4.0,
+    'The Fault in Our Stars': 5.0,
+    'Twilight': 5.0,
+    'The Twilight Saga: Breaking Dawn Part 1': 3.0,
+    'The Twilight Saga: Breaking Dawn Part 2': 3.0
+  },
+  'Sara': {
+    'The Notebook': 5.0,
+    'The Fault in Our Stars': 4.0,
+    'Twilight': 5.0,
+    'The Twilight Saga: Eclipse': 3.0,
+    'Casa Comigo': 4.5,
+    'De repente 30': 5.0
+  }
+}
---
+++ main.py
@@ -0,0 +1,4 @@
+import dicionario
+import math
+
+def 
